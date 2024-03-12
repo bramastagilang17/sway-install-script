cb5d8fd5daf84849b5beea1fc3591d409a540454
Putu Gilang Bramasta <bramastagilang17@gmail.com>
2024-03-12 12:49:29 +0800

    add theme install script

---
+++ themes/orchis-theme-install.sh
@@ -0,0 +1,5 @@
+# install theme
+git clone https://github.com/vinceliuice/Orchis-theme.git orchis-theme
+cd orchis-theme
+./install.sh --tweaks solid compact black -l
+
---
+++ themes/qogir-icons-install.sh
@@ -0,0 +1,8 @@
+# install icons
+git clone https://github.com/vinceliuice/Qogir-icon-theme.git qogir-icons
+cd qogir-icons
+./install.sh
+
+# install cursor
+cd src/cursors
+./install.sh
