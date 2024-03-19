abd9ab0e6962fc37ae2b4498ddb2a97697ce0d86
Gilang Bramasta <bramastagilang17@gmail.com>
2024-03-20 00:08:42 +0800

    v3

--- themes/orchis-theme-install.sh
+++
@@ -1,5 +0,0 @@
-# install theme
-git clone https://github.com/vinceliuice/Orchis-theme.git orchis-theme
-cd orchis-theme
-./install.sh --tweaks solid compact black -l
-
--- themes/qogir-icons-install.sh
+++
@@ -1,8 +0,0 @@
-# install icons
-git clone https://github.com/vinceliuice/Qogir-icon-theme.git qogir-icons
-cd qogir-icons
-./install.sh
-
-# install cursor
-cd src/cursors
-./install.sh
